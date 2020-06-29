bab38e0943f257ca7e713af63c5a1994a4625606
AnShiko <67584469+AnShiko@users.noreply.github.com>
2020-06-29 15:22:55 +0300

    Initial commit

---
+++ src/main/java/com/geekbrains/work4/Apple.java
@@ -0,0 +1,7 @@
+package com.geekbrains.work4;
+
+public class Apple extends Fruit {
+    public Apple() {
+        super(50);
+    }
+}
---
+++ src/main/java/com/geekbrains/work4/Orange.java
@@ -0,0 +1,7 @@
+package com.geekbrains.work4;
+
+public class Orange extends Fruit {
+    public Orange() {
+        super(75);
+    }
+}
